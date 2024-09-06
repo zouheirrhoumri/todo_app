749c26209a4336c2c7850fa865622eb373e79b12
zouheirrhoumri <144812016+zouheirrhoumri@users.noreply.github.com>
2024-09-06 14:58:54 +0100

    done with add function

--- todoapp.c
+++ todoapp.c
@@ -18,3 +18,43 @@ typedef struct tache
 tache t[100];
 int ID = 0;
 int dim = 0;
+
+
+void ajouter_tache(tache t[])
+{
+    int choix2;
+    t[dim].id = ID + 1;
+    printf("\t\ttitre : ");
+    scanf(" %[^\n]", t[dim].titre);
+    strupr(t[dim].titre);
+    printf("\t\tdescription : ");
+    scanf(" %[^\n]", t[dim].desc);
+    do
+    {
+        printf("\t\tStatus du tache :\n \t1- A realiser | 2- En cours | 3- Finalisee  : ");
+        scanf("%d", &choix2);
+
+    } while (choix2 <= 0 || choix2 >= 4);
+    switch (choix2)
+    {
+    case 1:
+        strcpy(t[dim].status, "A realiser");
+        break;
+    case 2:
+        strcpy(t[dim].status, "En cours");
+        break;
+    case 3:
+        strcpy(t[dim].status, "Finalisee");
+        break;
+    default:
+        printf("valeur invalide.\n");
+    }
+    do
+    {
+        printf("\t\tsaisir le deadline :");
+        scanf("%d/%d/%d", &t[dim].deadline.jour, &t[dim].deadline.mois, &t[dim].deadline.annee);
+    } while (t[dim].deadline.jour <= 0 || t[dim].deadline.jour >= 32 || t[dim].deadline.mois <= 0 || t[dim].deadline.mois >= 13 || t[dim].deadline.annee <= 0);
+    dim++;
+    ID++;
+}
+
