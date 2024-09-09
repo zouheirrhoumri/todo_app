8035927681c864ebb8fd364682afbd0106589a19
zouheirrhoumri <144812016+zouheirrhoumri@users.noreply.github.com>
2024-09-09 12:51:22 +0100

    added the files

--- todoapp.c
+++ todoapp.c
@@ -19,16 +19,17 @@ tache t[100];
 int ID = 0;
 int dim = 0;
 
-
-void ajouter_tache(tache t[])
+void ajouter_tache()
 {
+    tache temp;
+
     int choix2;
-    t[dim].id = ID + 1;
+    temp.id = ID + 1;
     printf("\t\ttitre : ");
-    scanf(" %[^\n]", t[dim].titre);
-    strupr(t[dim].titre);
+    scanf(" %[^\n]", temp.titre);
+    strupr(temp.titre);
     printf("\t\tdescription : ");
-    scanf(" %[^\n]", t[dim].desc);
+    scanf(" %[^\n]", temp.desc);
     do
     {
         printf("\t\tStatus du tache :\n \t1- A realiser | 2- En cours | 3- Finalisee  : ");
@@ -38,23 +39,42 @@ void ajouter_tache(tache t[])
     switch (choix2)
     {
     case 1:
-        strcpy(t[dim].status, "A realiser");
+        strcpy(temp.status, "A realiser");
         break;
     case 2:
-        strcpy(t[dim].status, "En cours");
+        strcpy(temp.status, "En cours");
         break;
     case 3:
-        strcpy(t[dim].status, "Finalisee");
+        strcpy(temp.status, "Finalisee");
         break;
     default:
         printf("valeur invalide.\n");
     }
+    int scan=0;
     do
     {
+        
         printf("\t\tsaisir le deadline :");
-        scanf("%d/%d/%d", &t[dim].deadline.jour, &t[dim].deadline.mois, &t[dim].deadline.annee);
-    } while (t[dim].deadline.jour <= 0 || t[dim].deadline.jour >= 32 || t[dim].deadline.mois <= 0 || t[dim].deadline.mois >= 13 || t[dim].deadline.annee <= 0);
-    dim++;
+        scan= scanf("%d/%d/%d", &temp.deadline.jour, &temp.deadline.mois, &temp.deadline.annee);
+       // printf("%d/%d/%d", temp.deadline.jour, temp.deadline.mois, temp.deadline.annee);
+    } while (scan != 3);
+    
+    
+   // } while (temp.deadline.jour <= 0 || temp.deadline.jour >= 32 || temp.deadline.mois <= 0 || temp.deadline.mois >= 13 || t[dim].deadline.annee <= 0);
+     // t[dim] = temp;
+   FILE *file = fopen("taches.txt", "ab");
+    if (file != NULL)
+    {
+        fwrite(&temp , sizeof(temp) , 1 , file);
+    }
+    else
+    {
+        printf("Erreur lors de l'ouverture du fichier\n");
+        return;
+    }
+    fclose(file);
+   //dim++ ;
+
     ID++;
 }
 
@@ -70,15 +90,40 @@ void ajouter_plus(tache t[])
     }
 }
 
-void afficher_tache(tache t[])
+void afficher_tache()
 {
+    tache temp;
     printf("\t\t+------------------------------------------------------------------------------+\n");
     printf("\t\t| Id |        Tache         |      Description     | Date limite |    Status   |\n");
     printf("\t\t+------------------------------------------------------------------------------+\n");
-    for (int i = 0; i < dim; i++)
-    {
+    // for (int i = 0; i < dim; i++)
+    // {
 
-        printf("\t\t| %-2d | %-20s | %-20s | %2d-%2d-%4d  | %-11s |\n", t[i].id, t[i].titre, t[i].desc, t[i].deadline.jour, t[i].deadline.mois, t[i].deadline.annee, t[i].status);
+    //     printf("\t\t| %-2d | %-20s | %-20s | %2d-%2d-%4d  | %-11s |\n", t[i].id, t[i].titre, t[i].desc, t[i].deadline.jour, t[i].deadline.mois, t[i].deadline.annee, t[i].status);
+    // }
+    FILE *file = fopen("taches.txt", "rb");
+
+        if (file != NULL)
+    {
+        while (fread(&temp , sizeof(temp) , 1 , file))
+    {
+           printf("\t\t| %-2d | %-20s | %-20s | %2d-%2d-%4d  | %-11s |\n", temp.id, temp.titre, temp.desc, temp.deadline.jour, temp.deadline.mois, temp.deadline.annee, temp.status);
+    
     }
+    }
+    else
+    {
+        printf("Erreur lors de l'ouverture du fichier\n");
+        return;
+    }
+    fclose(file);
+
+    
+    
     printf("\t\t+------------------------------------------------------------------------------+\n");
 }
+ int main(){
+   //   ajouter_tache();
+   afficher_tache();
+    return 0;
+ }
